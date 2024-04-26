9e74f8532c68b15e8635eb8083ea8b598381c7b6
James Leahy Stephens <jaystephens1124@gmail.com>
2024-04-26 00:05:08 -0400

    Booking

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -12,47 +12,67 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper";
+import App from "../App";
+
+var thisCard;
+var listing_id;
 
 export function Listing() {
-    const location = useLocation();
-    const card = location.state;
-    console.log(card);
-    return (
-      <>
-       <div className="card-box1">
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={15}
-          loop={true}
-          mousewheel={true}
-          cssMode={true}
-          pagination
-          modules={[Pagination, Navigation]}
-          className="swiper-container"
-        >
-            <SwiperSlide key={0}>
-              <img src={card.listing_image} className="card-img" />
-            </SwiperSlide>
-        </Swiper>
-        <div className="card-info-flex">
-          <h3 className="card-title">{card.listing_name}</h3>
+  const location = useLocation();
+  const card = location.state;
+  console.log(card);
+  listing_id = card.id;
+  thisCard = card;
+  console.log(thisCard);
+  return (
+    <>
+      <div className="card-box1">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={15}
+        loop={true}
+        mousewheel={true}
+        cssMode={true}
+        pagination
+        modules={[Pagination, Navigation]}
+        className="swiper-container"
+      >
+          <SwiperSlide key={0}>
+            <img src={card.listing_image} className="card-img" />
+          </SwiperSlide>
+      </Swiper>
+      <div className="card-info-flex">
+        <h3 className="card-title">{card.listing_name}</h3>
+      </div>
+      <p style={{ margin: 0, color: "var(--font-grey)" , }}>{card.address}, {card.property_city}, {card.property_country}</p>
+      <p style={{ margin: 0, color: "var(--font-grey)" }}>{card.listing_desc}</p>
+      <p style={{ margin: "0.2rem", fontSize: "1rem", color: "var(--black" }}>
+        <span style={{ fontWeight: "600" }}>${card.price_per_night}</span> night</p>
+        <div className="input-bar">
+        Start Date
+          <input className="startDateInput"></input>
         </div>
-        <p style={{ margin: 0, color: "var(--font-grey)" , }}>{card.address}, {card.property_city}, {card.property_country}</p>
-        <p style={{ margin: 0, color: "var(--font-grey)" }}>{card.listing_desc}</p>
-        <p style={{ margin: "0.2rem", fontSize: "1rem", color: "var(--black" }}>
-          <span style={{ fontWeight: "600" }}>${card.price_per_night}</span> night</p>
-          <div className="input-bar">
-          Start Date
-            <input className="startDateInput"></input>
-          </div>
-          <div className="input-bar">
-          End Date
-            <input className="endDateInput"></input>
-          </div>
-          <div className="button-div">
-            <button className="bookButton">Book Now</button>
-          </div>
+        <div className="input-bar">
+        End Date
+          <input className="endDateInput"></input>
         </div>
-      </>
-    );
-  }
+        <div className="button-div">
+          <button className="bookButton" onClick={(booking)}>Book Now</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function booking()
+{
+  var startDate = document.querySelector(".startDateInput").value;
+  var endDate = document.querySelector(".endDateInput").value;
+  var queryString = `INSERT INTO project.Booking (listing_id, start_date, end_date, renter_id) VALUES (${thisCard.id}, '${startDate}', '${endDate}',${App.account_id});`;
+  var encoded = encodeURIComponent(queryString);
+  var url=`http://localhost:3005/sql?data=${encoded}`
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
